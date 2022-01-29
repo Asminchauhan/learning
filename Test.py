265a581241ef14f0588e0a99518eef0f519c8fba
Asmin <asmin@live.com>
2022-01-29 13:16:27 +0530

    changes are done

--- Test.py
+++ Test.py
@@ -1,1 +1,11 @@
-print("hello")
+#name = input('What is your name?')
+#age = input('What is age?')
+
+#print(name + "is" + age)
+
+#x = 3
+#num = 17
+#print(num % x)
+
+
+print(sum:=int(input("What is the first number?"))+int(input("What is the second number?")))
