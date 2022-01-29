a5ceb05b17a4c1417b4936f9b032cb50416bc5b7
Asmin <asmin@live.com>
2022-01-29 13:05:05 +0530

    adding file from personal PC

---
+++ Strings_and_variables.py
@@ -0,0 +1,37 @@
+from unicodedata import name
+
+
+print('Python is Fun!')
+
+print('Brian\'s mother: He\'s not an angel')     #using backslash to have apostrophe
+
+print('Asmin\nChauhan')  # \n to add new line 
+
+print('Asmin\tChauhan')  # \t to add tab
+
+print("""This 
+is a multiple 
+line text""")           #" " " Three quotes will give string in each line automatically
+
+print("Spam" + 'eggs')
+print('2'+ '2')             #Concatenation of strings 
+
+print("spam" *3)
+print(4 *'2')               #Multiplication of Strings to get repeated values 
+
+#input() to get input from the user
+
+Name = input('What is you name?')
+print(Name + ' is learning Python!')
+
+
+#The inputs can be converted to number using int(), to a string using str() and to float using float()
+
+#In-place operators can be used for any numerical operation (+, -, *, /, %, **, //)
+
+print(num:=int(input('Enter the number')))              # := Walrus operator allows to assign values without existing variable
+
+
+
+
+
